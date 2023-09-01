5fb2a1ecb77f1c4bdcf5204832c8d3850d6f2ef7
yang <1830574935@qq.com>
2023-09-01 18:07:14 +0800

    ansi

---
+++ ansi/getchar.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+/* count digits, white space, others */
+int main()
+{
+int c, i, nwhite, nother;
+int ndigit[10];
+nwhite = nother = 0;
+for (i = 0; i < 10; ++i)
+ndigit[i] = 0;
+while ((c = getchar()) != EOF)
+if (c >= '0' && c <= '9')
+++ndigit[c-'0'];
+else if (c == ' ' || c == '\n' || c == '\t')
+++nwhite;
+else
+++nother;
+printf("digits =");
+for (i = 0; i < 10; ++i)
+printf(" %d", ndigit[i]);
+printf(", white space = %d, other = %d\n",nwhite, nother);
+ return 0;
+}
+/* #include<stdio.h> */
+
+/* int main( ) */
+/* { */
+/*   int c = 0; */
+/*   printf("%d", c); */
+/*   return 0; */
+/* } */
+
+/* for (i = 0; i < N; ++i) { */
+  
+/*  } */
+/* include */
+/* /\* TODO:  *\/ */
+/* int main(int argc, char *argv[]) */
+/* { */
+  
+/*   return 0; */
+/* } */
+/* #include <math.h> */
+/* fgets(variable, size, stdin); */
+/* #include <stdio.h>typedef union { */
+
+/* } jkk; */
+/* /\* FIXME: for (int i = 0; i < MAXIMUM; ++i) { */
+/*    kjfla */
+/*    } *\/ */
+/* while (i , i + 1) { */
+  
+/*  } */
+/* #ifdef klkjjlajf */
+
+
+
+/* #endif // klkjjlajf */
+/* #define */
+/* malloc(sizeof() * 4); */
+/* __attribute__((__packed__)) */
+/* printf("%d %c %e",a ,);strstr(sfsfsf, string); */
+/* // -*- compile-command: "gcc -Wall -o dest file" -*- */
+/* # -*- mode:conf; -*- */
+/* 		 /\* comment *******************************************************************\/ */
+/* 		 /\********************************************************************************************************************\/ */
+/* 		 /\* klljlsfjslfjslfjsldfjsldfjsldfjslfjslfjslfjslfjslfjslfjslfjslfjslfjsldjfslfjslfjlsfjsdlfjlsjfslfjlsjflsjflsjflsf *\/ */
+/* 		 /\********************************************************************************************************************\/ */
+/* switch (expr) { */
+/*  case constexpr: { */
+/*    klj */
+/*    break; */
+/*  } */
+/*  default: */
+/*    break; */
+/*  } */
+/* /\** */
+/*  * @brief      function description */
+/*  * */
+/*  * @details    detailed description */
+/*  * */
+/*  * @param      param */
+/*  * */
+/*  * @return     return type */
+/*  *\/ */
+/* /\** */
+/*  *  \brief function description */
+/*  * */
+/*  *  Detailed description */
+/*  * */
+/*  *  \param param */
+/*  *  \return return type */
+/*  *\/ */
+
+/* /\** */
+/*  * @brief      function description */
+/*  * */
+/*  * @details    detailed description */
+/*  * */
+/*  * @param      param */
+/*  * */
+/*  * @return     return type */
+/*  *\/ */
+/* #include<stdio.h> */
